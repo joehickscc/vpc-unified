0ee199778f0972d93444e3f96f5f73686b1e8ba1
Joe Hicks <joe@cloudcoreo.com>
2017-02-07 13:18:34 -0800

    Change order of routetable and subnet

--- services/config.rb
+++ services/config.rb
@@ -10,6 +10,21 @@ coreo_aws_vpc_vpc "${VPC_NAME}${SUFFIX}" do
 end
 
 ######################################################################
+## create a routetable for the public subnet
+## route everything to the internet gateway
+######################################################################
+
+coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}${SUFFIX}" do
+  action :sustain
+  vpc "${VPC_NAME}${SUFFIX}"
+  routes [
+             { :from => "0.0.0.0/0", :to => "${VPC_NAME}${SUFFIX}", :type => :igw }
+        ]
+  number_of_tables 1
+  region "${REGION}"
+end
+
+######################################################################
 ## Create a Public Subnet
 ##
 ## cidr will be split up among all zones specified in "number_of_zones"
@@ -29,17 +44,13 @@ coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}${SUFFIX}" do
 end
 
 ######################################################################
-## create a routetable for the public subnet
-## route everything to the internet gateway
+## create a routetable for the private subnet
 ######################################################################
 
-coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}${SUFFIX}" do
-  action :sustain
+coreo_aws_vpc_routetable "${PRIVATE_ROUTE_NAME}${SUFFIX}" do
+  action :create
   vpc "${VPC_NAME}${SUFFIX}"
-  routes [
-             { :from => "0.0.0.0/0", :to => "${VPC_NAME}${SUFFIX}", :type => :igw }
-        ]
-  number_of_tables 1
+  number_of_tables ${PRIVATE_ROUTE_NUM_TABLES}
   region "${REGION}"
 end
 
@@ -53,17 +64,6 @@ coreo_aws_vpc_subnet "${PRIVATE_SUBNET_NAME}${SUFFIX}" do
   percent_of_vpc_allocated 50
   route_table "${PRIVATE_ROUTE_NAME}${SUFFIX}"
   vpc "${VPC_NAME}${SUFFIX}"
-  region "${REGION}"
-end
-
-######################################################################
-## create a routetable for the private subnet
-######################################################################
-
-coreo_aws_vpc_routetable "${PRIVATE_ROUTE_NAME}${SUFFIX}" do
-  action :create
-  vpc "${VPC_NAME}${SUFFIX}"
-  number_of_tables ${PRIVATE_ROUTE_NUM_TABLES}
   region "${REGION}"
 end
 
